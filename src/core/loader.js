58079b30963ca2bb72f181e90d1b7c63d0e4dbb3
xiaopan42 <junrui0422@gmail.com>
2025-10-21 22:11:40 +0800

    修改

--- src/core/loader.js
+++ src/core/loader.js
@@ -1,10 +1,24 @@
 import { REST, Routes } from "discord.js"
+import fg from 'fast-glob'
 
 const updateSlashCommands = () => {
     const rest = new REST({version:10}).setToken(process.env.TOKEN)
     rest.put(
         Routes.applicationGuildCommands(
-            process.env.APPLICATION_ID  
-        )
+            process.env.APPLICATION_ID,
+            '1429780753629057037'   
+        ),
+        {
+            body:{}
+        }
     )
+}
+
+
+export const loadCommands = async() => {
+    const files = await fg('./src/commands/**/index.js')
+    for(const file of files) {
+    const cmd = await import(file)
+    console.log(cmd)
+    }
 }
